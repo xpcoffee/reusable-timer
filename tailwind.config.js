607420891424e8ca6de78da49c457a378c2732e9
Emerick Bosch <xpc.dev@gmail.com>
2020-01-29 23:03:58 +0200

    Embolden the styling
    This shoud present better on mobile. Also adds in styling for focus and click events.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {}
+    },
+    variants: {
+        backgroundColor: ["responsive", "hover", "focus", "active"],
+        textColor: ["responsive", "hover", "focus", "active"],
+        borderStyle: ["focus"]
+    },
+    plugins: []
+}

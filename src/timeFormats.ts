ab4982ef7fc9114f68fdbd2740e28d2ccc891a60
Emerick Bosch <xpc.dev@gmail.com>
2020-02-08 00:40:05 +0200

    Deal with 0s edgecase when formatting the counter
    Dividing by 0 is apparently bad.

--- src/timeFormats.ts
+++ src/timeFormats.ts
@@ -5,12 +5,20 @@ const HOURS_PER_DAY = 24;
 const SECONDS_PER_DAY = HOURS_PER_DAY * SECONDS_PER_HOUR;
 
 export function minuteFormat(seconds: number): string {
+    if (seconds === 0) {
+        return "0m 0s"
+    }
+
     const secs = seconds % SECONDS_PER_MINUTE;
     const minutes = Math.floor(seconds / SECONDS_PER_MINUTE);
     return `${minutes}m ${secs}s`
 }
 
 export function hourFormat(seconds: number): string {
+    if (seconds === 0) {
+        return "0h 0m 0s"
+    }
+
     const secs = seconds % SECONDS_PER_MINUTE;
     const minutes = Math.floor(seconds / SECONDS_PER_MINUTE) % MINUTES_PER_HOUR;
     const hours = Math.floor(seconds / SECONDS_PER_HOUR);
@@ -18,6 +26,10 @@ export function hourFormat(seconds: number): string {
 }
 
 export function dayFormat(seconds: number): string {
+    if (seconds === 0) {
+        return "0d 0h 0m 0s"
+    }
+
     const secs = seconds % SECONDS_PER_MINUTE;
     const minutes = Math.floor(seconds / SECONDS_PER_MINUTE) % MINUTES_PER_HOUR;
     const hours = Math.floor(seconds / SECONDS_PER_HOUR) % HOURS_PER_DAY;

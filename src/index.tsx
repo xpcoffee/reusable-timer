cd0c9cb3afa75c59ed885bd98af91efec33d0054
Emerick Bosch <xpc.dev@gmail.com>
2020-02-09 20:15:31 +0200

    Replace FontAwesome dependency with raw SVGs
    FontAwesome was bumping the bundle up to ~2MB.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "preact/hooks";
 import './style.css';
 import { createTimer, Timer } from "./timer";
 import { minuteFormat, hourFormat, dayFormat } from "./timeFormats";
-import { faEdit, faTimesCircle, IconDefinition, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
     try {
@@ -92,24 +91,21 @@ interface CountDisplayState {
 }
 
 function EditCountButton({ class: classes, onClick }: { class?: string, onClick?: () => void }) {
-    return <button aria-label="Edit time" class={`m-4 border-transparent focus:shadow-outline focus:outline-none ${classes}`} onClick={onClick}><FontAwesomeIcon icon={faEdit}></FontAwesomeIcon></button>
+    return <button aria-label="Edit time" class={`m-4 border-transparent focus:shadow-outline focus:outline-none ${classes}`} onClick={onClick}>
+        <svg class="fill-current text-gray-200" viewBox="0 0 576 512" style="width: 50px; height: 50px;"><path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"></path></svg>
+    </button>
 }
 
 function CancelEditCountButton({ class: classes, onClick }: { class?: string, onClick?: () => void }) {
-    return <button aria-label="Cancel edit" class={`m-4 border-transparent focus:shadow-outline focus:outline-none ${classes}`} onClick={onClick}><FontAwesomeIcon icon={faTimesCircle}></FontAwesomeIcon></button>
+    return <button aria-label="Cancel edit" class={`m-4 border-transparent focus:shadow-outline focus:outline-none ${classes}`} onClick={onClick}>
+        <svg class="fill-current text-gray-200" viewBox="0 0 512 512" style="width: 50px; height: 50px;"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path></svg>
+    </button>
 }
 
 function ConfirmEditCountButton({ class: classes, onClick }: { class?: string, onClick?: () => void }) {
-    return <button aria-label="Confirm edit" class={`m-4 border-transparent focus:shadow-outline focus:outline-none ${classes}`} onClick={onClick}><FontAwesomeIcon icon={faCheckCircle}></FontAwesomeIcon></button>
-}
-
-function FontAwesomeIcon({ icon }: { icon: IconDefinition }) {
-    const pathData = icon.icon[4]
-    let paths: string[] = ([] as string[]).concat(pathData);
-
-    return <svg class="fill-current text-gray-200" style={{ width: "50px", height: "50px" }} viewBox={`0 0 ${icon.icon[0] /*width*/} ${icon.icon[1] /*height*/}`} >
-        {paths.map(path => <path d={path} />)}
-    </svg>
+    return <button aria-label="Confirm edit" class={`m-4 border-transparent focus:shadow-outline focus:outline-none ${classes}`} onClick={onClick}>
+        <svg class="fill-current text-gray-200" viewBox="0 0 512 512" style="width: 50px; height: 50px;"><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>
+    </button>
 }
 
 const COUNT_DISPLAY_STATES: { [key: string]: CountDisplayState } = {
